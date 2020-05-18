2b5b3788010ea3326f4166c610c2be44c0dc11e7
pavan varma <pavan.vadapalli009@gmail.com>
2020-05-18 13:28:42 +0530

    updated

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,6 +3,7 @@ const request = require('request');
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1IjoicGF2MDAxIiwiYSI6ImNrOThkdWF5dTA3ejMzbnBsNDM1eW01MDcifQ.UIOSOEArLNd5je0X2qmi1A&limit=1';
     request({ url, json:true }, (error, {body}) => {
+        console.log(body.features[0])
         if(error)
         {
             callback('unable to connect to location service', undefined);
